f32368ff3f5cb12ba82b77f570204ec569bcfb2a
Sanjay <svadivel@rythmos.com>
2024-11-06 10:51:49 +0530

    redshift start completed

---
+++ aws_practice/redshift_start.py
@@ -0,0 +1,18 @@
+# Write a Python script to start a Redshift cluster if it is currently stopped.
+import boto3
+redshift_client = boto3.client('redshift', region_name='us-east-1')
+
+try:
+    response = redshift_client.describe_clusters()
+    clusters = response.get('Clusters', [])
+    if clusters:
+        print("Amazon Redshift Clusters:")
+        for cluster in clusters:
+            cluster_identifier = cluster['ClusterIdentifier']
+            cluster_status = cluster['ClusterStatus']
+            print(f"Cluster Identifier: {cluster_identifier}, Status: {cluster_status}")
+    else:
+        print("No Redshift clusters found in your account.")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
